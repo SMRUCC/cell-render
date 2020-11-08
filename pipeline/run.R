24f923ba70354456b254686ba9d8ba57b197204c
xieguigang <xie.guigang@gcmodeller.org>
2020-11-09 02:25:02 +0800

    no changed?

--- pipeline/run.R
+++ pipeline/run.R
@@ -3,5 +3,16 @@ imports "../package/01_background.R";
 setwd(dirname(!script$dir));
 
 let output_dir as string = "demo";
+let overrides as boolean = FALSE;
 
-makePtf(`${output_dir}/annotation/uniprot-taxonomy_3702.xml`, `${output_dir}/annotation/background.ptf`);
+let background_ptf as string = `${output_dir}/annotation/background.ptf`;
+let HTS as string = `${output_dir}/raw/uniprot.csv`;
+
+if (overrides || !file.exists(background_ptf)) {
+	makePtf(`${output_dir}/annotation/uniprot-taxonomy_3702.xml`, background_ptf);
+}
+
+`${output_dir}/raw/all_counts.csv`
+:> unifyId(background_ptf) 
+:> write.csv(file = HTS)
+;
