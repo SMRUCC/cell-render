ad2b3a2e8a8a1bdd41b6771a61addbdd8e073d72
xieguigang <xie.guigang@gcmodeller.org>
2020-11-09 19:38:34 +0800

    run dep t.test analysis

--- package/03_dep.R
+++ package/03_dep.R
@@ -1,14 +1,19 @@
 imports "visualPlot" from "visualkit";
-imports "geneExpression" from "phenotype_kit";
+imports ["geneExpression", "sampleInfo"] from "phenotype_kit";
 
-let run_dep as function(matrix, sampleinfo, compares, output_dir) {
+let run_dep as function(matrix, sampleInfo, compares, output_dir) {
 	let as_label = compare_dir -> `${compare_dir$treatment} vs ${compare_dir$control}`;
+	let	sampleinfo = read.sampleinfo(sampleInfo); 
 
 	for(compare_dir in compares) {
+		print(`run dep analysis of '${as_label(compare_dir)}'...`);
+	
 		let dep = deg.t.test(matrix, sampleinfo, compare_dir$treatment, compare_dir$control);
-		let compare_out = `${output_dir}/${as_label(compare_dir)}`;
+		let compare_out = `${output_dir}/analysis/03.dep_analysis/${as_label(compare_dir)}`;
 		
 		write.csv(dep, file = `${compare_out}/pvalue.csv`);
-		volcano.plot(dep) :> save.graphics(file = `${compare_out}/volcano.png`);
+		# volcano.plot(dep) :> save.graphics(file = `${compare_out}/volcano.png`);
+		
+		print("done!");
 	}
 }
--- pipeline/run.R
+++ pipeline/run.R
@@ -2,11 +2,20 @@
 imports "../package/init.R";
 # data analysis and pre-processing components
 imports "../package/01_background.R";
+imports "../package/03_dep.R";
 
 setwd(dirname(!script$dir));
 
 let output_dir as string = "demo";
 let overrides as boolean = ?"--overrides" || FALSE;
+let sample_info as string = `${output_dir}/sampleInfo.csv`;
+let analysis = list(
+	a = list(treatment = "C6", control = "C9"),
+	b = list(treatment = "C6", control = "I56"),
+	c = list(treatment = "C6", control = "I59"),
+	d = list(treatment = "C6", control = "I86"),
+	e = list(treatment = "C6", control = "I89")
+);
 
 let background_ptf as string = `${output_dir}/annotation/background.ptf`;
 let HTS as string = `${output_dir}/raw/uniprot.csv`;
@@ -25,3 +34,10 @@ if (overrides || !file.exists(background_ptf)) {
 ;
 
 init_workspace(output_dir);
+
+run_dep(
+	matrix     = load.expr(HTS), 
+	sampleInfo = sample_info, 
+	compares   = analysis, 
+	output_dir = output_dir
+);
