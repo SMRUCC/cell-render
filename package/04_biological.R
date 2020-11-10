be68fba72ff9bc6bad6829d655eea7c7d7f54d3d
xieguigang <xie.guigang@gcmodeller.org>
2020-11-10 20:43:25 +0800

    add debug echo

--- package/04_biological.R
+++ package/04_biological.R
@@ -6,12 +6,15 @@ let patterns_plot as function(workspace) {
 		let pvalue_cut  = `${workspace$dirs$dep_analysis}/${as_label(compare_dir)}/pvalue_cut.csv`;
 		let cluster_out = `${workspace$dirs$biological_analysis}/${as_label(compare_dir)}`;
 		
-		pvalue_cut = read.csv(pvalue_cut);
+		pvalue_cut = read.csv(pvalue_cut, row_names = 1);
 		pvalue_cut[, "FC.avg"]  = NULL;
 		pvalue_cut[, "p.value"] = NULL;
 		pvalue_cut[, "is.DEP"]  = NULL;
 		pvalue_cut[, "log2FC"]  = NULL;
 		pvalue_cut[, "FDR"]     = NULL;
+		
+		print(`[${as_label(compare_dir)}] previews of the different expression proteins:`);
+		print(head(pvalue_cut));
 		
 		let patterns = pvalue_cut 
 		:> load.expr
