ba91a6e96d3088f6545fee17ad0dc14ff670f101
關倩倩 <evia@lilithaf.me>
2020-11-21 18:20:51 +0800

    restore removed script updates caused by the git bugs

--- package/04_biological.R
+++ package/04_biological.R
@@ -1,35 +1,57 @@
 imports ["geneExpression", "sampleInfo"] from "phenotype_kit";
 imports "visualPlot" from "visualkit";
 
+#' create clusters for biological function analysis
+#' 
+#' @param workspace the analysis workspace object, it should contains a 
+#'     directory path list which can be used for read dep pvalue_cut 
+#'     result.
+#' 
 let patterns_plot as function(workspace) {
-	for(compare_dir in workspace$analysis) {
-		let pvalue_cut  = `${workspace$dirs$dep_analysis}/${as_label(compare_dir)}/pvalue_cut.csv`;
-		let cluster_out = `${workspace$dirs$biological_analysis}/${as_label(compare_dir)}`;
-		
-		pvalue_cut = read.csv(pvalue_cut, row_names = 1);
-		pvalue_cut[, "FC.avg"]  = NULL;
-		pvalue_cut[, "p.value"] = NULL;
-		pvalue_cut[, "is.DEP"]  = NULL;
-		pvalue_cut[, "log2FC"]  = NULL;
-		pvalue_cut[, "FDR"]     = NULL;	
-				
-		print(`[${as_label(compare_dir)}] previews of the different expression proteins:`);
-		print(head(pvalue_cut));
-		
-		let patterns = pvalue_cut 
-		:> load.expr
-		:> relative
-		:> expression.cmeans_pattern(dim = [3,3], fuzzification = 2, threshold = 0.001)
-		;
-		
-		patterns 
-		:> plot.expression_patterns(size = [6000, 4500])
-		:> save.graphics(`${cluster_out}/patterns.png`)
-		;
-		
-		patterns
-		:> cmeans_matrix(kmeans_n = 3)
-		:> write.csv(file = `${cluster_out}/clusters.csv`)
-		;
-	}	
+	lapply(workspace$analysis, compare_dir -> workspace :> create_pattern(compare_dir));
+}
+
+#' create cluster for a specific analysis compare groups
+#' 
+#' @param workspace the workspace object that contains the directory path
+#'     data for read pvalue_cut dep analysis result
+#' @param compare_dir the analsysis design groups, is a list object that should contains
+#'     a slot name \code{treatment} and a slot name \code{control}
+#' 
+let create_pattern as function(workspace, compare_dir) {
+	let pvalue_cut  = `${workspace$dirs$dep_analysis}/${as_label(compare_dir)}/pvalue_cut.csv`;
+	let cluster_out = `${workspace$dirs$biological_analysis}/${as_label(compare_dir)}`;
+	
+	# removes all of the unnecessary information
+	# create a expression matrix that contains
+	# only of the protein expression normalization
+	# data
+	pvalue_cut = read.csv(pvalue_cut, row_names = 1);
+	pvalue_cut[, "FC.avg"]  = NULL;
+	pvalue_cut[, "p.value"] = NULL;
+	pvalue_cut[, "is.DEP"]  = NULL;
+	pvalue_cut[, "log2FC"]  = NULL;
+	pvalue_cut[, "FDR"]     = NULL;	
+			
+	print(`[${as_label(compare_dir)}] previews of the different expression proteins:`);
+	print(head(pvalue_cut));
+	
+	# run cmeans clustering
+	let patterns = pvalue_cut 
+	:> load.expr
+	:> relative
+	:> expression.cmeans_pattern(dim = [3,3], fuzzification = 2, threshold = 0.001)
+	;
+	
+	# do data visualization
+	patterns 
+	:> plot.expression_patterns(size = [6000, 4500])
+	:> save.graphics(`${cluster_out}/patterns.png`)
+	;
+	
+	# and then save the cluster result matrix
+	patterns
+	:> cmeans_matrix(kmeans_n = 3)
+	:> write.csv(file = `${cluster_out}/clusters.csv`)
+	;
 }
