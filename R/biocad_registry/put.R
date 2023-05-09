6efc9d5b626e7c1dd2e11c1ce68bbd9506798ef6
xieguigang <xie.guigang@gcmodeller.org>
2023-05-09 22:05:56 +0800

    move web services file to biocad_registry module

--- R/biocad_registry/put.R
+++
@@ -1,9 +0,0 @@
-imports "http" from "webKit";
-
-const taxonomic_group.create = function(name, note = "") {
-    const base = getOption("biocad");
-    const url  = `${base}/registry/put/taxonomic/`;
-    const resp = requests.post(url, list(name, note)) |> http::content();
-
-    str(resp);
-}
--- R/zzz.R
+++ R/zzz.R
@@ -2,5 +2,5 @@ require(GCModeller);
 require(WorkflowRender);
 
 const .onLoad = function() {
-    options(biocad = "http://biocad.cloud:8848");
+    
 }
