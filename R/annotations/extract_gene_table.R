0f87232b75140e752673a1907feab8fe6f565725
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-05-14 14:05:01 +0800

    the locis object is null

--- R/annotations/extract_gene_table.R
+++ R/annotations/extract_gene_table.R
@@ -13,6 +13,7 @@ const extract_gene_table = function(app, context) {
     # get workspace dir path of current workflow app module
     const workdir = WorkflowRender::workspace(app);
     const verbose = as.logical(getOption("verbose"));
+    const upstream_size = context$configs$up_len || 150;
 
     if (verbose) {
         print("app workspace for extract gene table:");
@@ -22,18 +23,28 @@ const extract_gene_table = function(app, context) {
     # extract the raw genomics fasta sequence
     const genomics_seq = origin.fasta(gbk);
     const genes = genome.genes(genome = gbk);
+
+    print("get genes table:");
+    print([genes]::Synonym);
+    print("bp size for parse the gene upstream loci:");
+    str(upstream_size);
+
     const locis = genes 
-    |> upstream(length = context$configs$up_len || 150) 
+    |> upstream(length = upstream_size) 
     |> lapply(l -> l, names = [genes]::Synonym)
-    |> lapply(loci -> cut_seq.linear(genomics_seq, loci, doNtAutoReverse = TRUE))
+    |> lapply(function(loci) {
+        cut_seq.linear(genomics_seq, loci, doNtAutoReverse = TRUE);
+    })
     ;
+
+    # export genomics context elements as feature table.
+    write.csv(genes, file = `${workdir}/genes.csv`);
+    write.PTT_tabular(gbk, file = `${workdir}/context.txt`);
 
     # extract sequence data and the gene context data for the
     # downstream transcript regulation network analysis
     write.fasta(genomics_seq, file = `${workdir}/source.fasta`);
     write.fasta(locis, file = `${workdir}/upstream_locis.fasta`);
-    write.csv(genes, file = `${workdir}/genes.csv`);
-    write.PTT_tabular(gbk, file = `${workdir}/context.txt`);
 
     # str(app);
     # str(src);
