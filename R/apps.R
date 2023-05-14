c6ab30f997fe1f616baa95b471a614f9b44b6dba
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-05-14 13:47:09 +0800

    add dev notes

--- R/apps.R
+++ R/apps.R
@@ -1,6 +1,10 @@
 #' Register the virtual cell network modelling workflows
 #' 
+#' @return this function has no return value
+#' 
 const annotation_workflow = function() {
+    print("Config the cellular graph network annotation & modelling workflow...");
+
     WorkflowRender::hook(app("extract_genbank_src", extract_gene_table));
     WorkflowRender::hook(app("extract_tfbs_motifs", tfbs_motif_scanning));
     WorkflowRender::hook(app("assemble_TRN", assemble_transcript_graph));
