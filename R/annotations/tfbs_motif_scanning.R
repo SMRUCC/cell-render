3019b46378f9b0f93dbb852c66c7006159faf7a7
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-05-14 16:12:25 +0800

    fix of the app reference

--- R/annotations/tfbs_motif_scanning.R
+++ R/annotations/tfbs_motif_scanning.R
@@ -4,7 +4,7 @@ imports "bioseq.patterns" from "seqtoolkit";
 #' Create the motif TFBS motif data from the gene upstream data
 #' 
 const tfbs_motif_scanning = function(app, context) {
-    const src_dir = WorkflowRender::workspace(extract_gene_table);
+    const src_dir = WorkflowRender::workspace("extract_genbank_src");
     const upstream_seq = read.fasta(`${src_dir}/upstream_locis.fasta`);
     const motifs = find_motifs(upstream_seq);
     const workdir = WorkflowRender::workspace(app);
