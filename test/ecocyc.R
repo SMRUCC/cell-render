58f3ea219a5416220ea81e73b653a3796d53aa05
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-06-06 21:22:03 +0800

    Create ecocyc.R

---
+++ test/ecocyc.R
@@ -0,0 +1,11 @@
+require(GCModeller);
+
+imports "compiler" from "vcellkit";
+imports "BioCyc" from "annotationKit";
+imports "GenBank" from "seqtoolkit";
+
+let genome_src = "E:\GCA_000005845.2_ASM584v2_genomic\source\NC_000913.gb";
+let vcell = "E:\UniProt\BioCyc\tier1\ecoli\25.1"
+|> open.biocyc()
+|> compile.biocyc(genomes = read.genbank(genome_src, repliconTable = TRUE))
+;
