662914cb366fc45b7423a2cce2c926a1a4dde1fa
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-10-20 12:02:35 +0800

    helper script of extract genbank file

--- R/annotations/extract_gene_table.R
+++ R/annotations/extract_gene_table.R
@@ -1,15 +1,10 @@
-imports "GenBank" from "seqtoolkit";
-imports "bioseq.fasta" from "seqtoolkit";
-imports "annotation.genomics" from "seqtoolkit";
-
 #' Extract the genbank source
 #' 
 #' @param app the current workflow app object
 #' @param context the workflow context object
 #' 
 const extract_gene_table = function(app, context) {
-    const src = context$configs$src;
-    const gbk = read.genbank(src);
+    const src = context$configs$src;    
     # get workspace dir path of current workflow app module
     const workdir = WorkflowRender::workspace(app);
     const verbose = as.logical(getOption("verbose"));
@@ -20,46 +15,6 @@ const extract_gene_table = function(app, context) {
         print(workdir);
     }
 
-    # extract the raw genomics fasta sequence
-    const genomics_seq = origin.fasta(gbk);
-    const genes = genome.genes(genome = gbk);
-    const gene_ids = [genes]::Synonym;
-
-    print("get genes table:");
-    print(gene_ids);
-    print("bp size for parse the gene upstream loci:");
-    str(upstream_size);
-
-    const locis = genes 
-    |> upstream(length = upstream_size) 
-    |> lapply(l -> l, names = gene_ids)
-    |> lapply(function(loci, i) {
-        let fa = cut_seq.linear(genomics_seq, loci, doNtAutoReverse = TRUE);
-        let id = gene_ids[i];
-
-        # tag the corresponding gene id to the
-        # loci site headers
-        fasta.headers(fa) = append(id, [fa]::Headers);
-        fa;
-    })
-    ;
-
-    if (verbose) {
-        print("view upstream locis:");
-        print(fasta.titles(locis));
-    }
-
-    # export genomics context elements as feature table.
-    write.csv(genes, file = `${workdir}/genes.csv`);
-    write.PTT_tabular(gbk, file = `${workdir}/context.txt`);
-
-    # extract sequence data and the gene context data for the
-    # downstream transcript regulation network analysis
-    write.fasta(genomics_seq, file = `${workdir}/source.fasta`);
-    write.fasta(locis, file = `${workdir}/upstream_locis.fasta`);
-
-    # str(app);
-    # str(src);
-    # str(context);
-    invisible(NULL);
+    CellRender::extract_gbff(src, workdir, upstream_size, 
+        verbose = verbose);
 }
---
+++ R/tools/extract_gbff.R
@@ -0,0 +1,51 @@
+imports "GenBank" from "seqtoolkit";
+imports "bioseq.fasta" from "seqtoolkit";
+imports "annotation.genomics" from "seqtoolkit";
+
+#' Helper function for extract the annotation features from genbank file
+#' 
+const extract_gbff = function(src, workdir = "./", upstream_size = 150, verbose = TRUE) {
+    let gbk = read.genbank(src);
+    # extract the raw genomics fasta sequence
+    let genomics_seq = origin.fasta(gbk);
+    let genes = genome.genes(genome = gbk);
+    let gene_ids = [genes]::Synonym;
+
+    print("get genes table:");
+    print(gene_ids);
+    print("bp size for parse the gene upstream loci:");
+    str(upstream_size);
+
+    const locis = genes 
+    |> upstream(length = upstream_size) 
+    |> lapply(l -> l, names = gene_ids)
+    |> lapply(function(loci, i) {
+        let fa = cut_seq.linear(genomics_seq, loci, doNtAutoReverse = TRUE);
+        let id = gene_ids[i];
+
+        # tag the corresponding gene id to the
+        # loci site headers
+        fasta.headers(fa) = append(id, [fa]::Headers);
+        fa;
+    })
+    ;
+
+    if (verbose) {
+        print("view upstream locis:");
+        print(fasta.titles(locis));
+    }
+
+    # export genomics context elements as feature table.
+    write.csv(genes, file = `${workdir}/genes.csv`);
+    write.PTT_tabular(gbk, file = `${workdir}/context.txt`);
+
+    # extract sequence data and the gene context data for the
+    # downstream transcript regulation network analysis
+    write.fasta(genomics_seq, file = `${workdir}/source.fasta`);
+    write.fasta(locis, file = `${workdir}/upstream_locis.fasta`);
+
+    # str(app);
+    # str(src);
+    # str(context);
+    invisible(NULL);
+}
