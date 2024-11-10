9eaafa682d10d2222af6d518cfccedbd2230685e
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-11-10 20:21:29 +0800

    create project file

---
+++ src/CellRender/Builder.vb
@@ -0,0 +1,10 @@
+
+Imports Microsoft.VisualBasic.Scripting.MetaData
+
+''' <summary>
+''' Helper functions for build virtualcell model
+''' </summary>
+<Package("Builder")>
+Public Module Builder
+
+End Module
