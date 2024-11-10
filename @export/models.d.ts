e9ea6b7253aa3044725d0d22de7f5412eeede623
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-11-10 20:58:18 +0800

    run package msbuild

---
+++ @export/models.d.ts
@@ -0,0 +1,24 @@
+﻿// export R# package module type define for javascript/typescript language
+//
+//    imports "models" from "biocad_registry";
+//
+// ref=biocad_registry.models@biocad_registry, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
+
+/**
+*/
+declare namespace models {
+   /**
+   */
+   function enzyme(biocad_registry: object, ec_number: string): any;
+   /**
+     * @param env default value Is ``null``.
+   */
+   function enzyme_function(biocad_registry: object, enzyme_id: string, ec_number: string, metabolites: any, env?: object): any;
+   /**
+   */
+   function subcellular_location(biocad_registry: object, name: string, topology: string): object;
+   /**
+     * @param desc default value Is ``''``.
+   */
+   function vocabulary_id(biocad_registry: object, term: string, category: string, desc?: string): object;
+}
---
+++ man/biocad_registry/models/models.md
@@ -0,0 +1,8 @@
+﻿# models
+
+
+
++ [vocabulary_id](models/vocabulary_id.1) 
++ [subcellular_location](models/subcellular_location.1) 
++ [enzyme_function](models/enzyme_function.1) 
++ [enzyme](models/enzyme.1) 
