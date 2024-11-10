b4cb6831a8c7bf41d2acabb27b9c934292f03a67
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-11-10 20:26:04 +0800

    run package msbuild

---
+++ @export/Builder.d.ts
@@ -0,0 +1,8 @@
+﻿// export R# package module type define for javascript/typescript language
+//
+//    imports "Builder" from "CellRender";
+//
+// ref=CellRender.Builder@CellRender, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
+
+/**
+*/
