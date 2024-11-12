42c69f555901578acb086e6f089b6798b320e6c8
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-11-12 18:45:28 +0800

    create compiler function

---
+++ R/biocad_registry/compiler.R
@@ -0,0 +1,14 @@
+#' Compile the virtual cell model from ncbi genbank file
+#' 
+#' @param cad_registry the mysql connection to the biocad registry
+#' @param gbff the file path to the template ncbi genbank file
+#' 
+const compile_genbank = function(cad_registry, gbff) {
+    imports "GenBank" from "seqtoolkit";
+
+    if (is.character(gbff)) {
+        gbff <- read.genbank(gbff);
+    }
+
+    
+}
--- test/workspace_2024_ecoli/fetch_model.R
+++ test/workspace_2024_ecoli/fetch_model.R
@@ -1,3 +1,4 @@
 require(CellRender);
 
 let cad_registry = CellRender::open_registry("root", 123456, host = "192.168.2.233");
+let template = "G:\biocad_registry\test\Escherichia coli str. K-12 substr. MG1655.txt";
