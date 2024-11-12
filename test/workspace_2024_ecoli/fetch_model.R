54844ce1dcec1e9ad514c51d5dc31088bc31a5f4
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-11-12 18:50:28 +0800

    fetch model

--- test/workspace_2024_ecoli/fetch_model.R
+++ test/workspace_2024_ecoli/fetch_model.R
@@ -2,3 +2,9 @@ require(CellRender);
 
 let cad_registry = CellRender::open_registry("root", 123456, host = "192.168.2.233");
 let template = "G:\biocad_registry\test\Escherichia coli str. K-12 substr. MG1655.txt";
+let model = cad_registry |> compile_genbank(gbff = template);
+
+model 
+|> xml()
+|> writeLines(con = file.path(@dir,"MG1655.xml"))
+;
