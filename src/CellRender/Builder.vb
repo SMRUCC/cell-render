a99baff996f0572e20fa74722704a3ca3e6e73a3
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-11-12 19:19:19 +0800

    create model file

--- src/CellRender/Builder.vb
+++ src/CellRender/Builder.vb
@@ -1,9 +1,13 @@
 Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports SMRUCC.genomics.Assembly.NCBI.GenBank
+Imports SMRUCC.genomics.ComponentModel.Annotation
 Imports SMRUCC.genomics.GCModeller.Assembly.GCMarkupLanguage.v2
 Imports SMRUCC.genomics.GCModeller.CompilerServices
 Imports SMRUCC.genomics.Metagenomics
 Imports SMRUCC.Rsharp.Runtime
+Imports SMRUCC.Rsharp.Runtime.Components
+Imports SMRUCC.Rsharp.Runtime.Internal.[Object]
 Imports SMRUCC.Rsharp.Runtime.Interop
 Imports SMRUCC.Rsharp.Runtime.Vectorization
 
@@ -21,16 +25,25 @@ Public Module Builder
                                     genes As Object,
                                     Optional env As Environment = Nothing) As Object
 
-        Dim geneIds As String() = CLRVector.asCharacter(genes)
-        Dim chromosome As New replicon
+        Dim template As GeneTable()
 
-        Return New VirtualCell With {
-            .properties = New [Property],
-            .taxonomy = New Taxonomy,
-            .genome = New Genome With {
-                .replicons = {chromosome}
-            }
-        }
+        If genes Is Nothing Then
+            Return Internal.debug.stop("the required template source should not be nothing!", env)
+        End If
+
+        If TypeOf genes Is GBFF.File Then
+            template = DirectCast(genes, GBFF.File).ExportGeneFeatures
+        Else
+            Dim pull As pipeline = pipeline.TryCreatePipeline(Of GeneTable)(genes, env)
+
+            If pull.isError Then
+                Return Message.InCompatibleType(GetType(GBFF.File), genes.GetType, env)
+            Else
+                template = pull.populates(Of GeneTable)(env).ToArray
+            End If
+        End If
+
+        Return New Compiler(register, template).CreateModel
     End Function
 
 End Module
--- src/CellRender/Compiler.vb
+++ src/CellRender/Compiler.vb
@@ -1,10 +1,27 @@
-﻿Public Class Compiler
+﻿Imports SMRUCC.genomics.ComponentModel.Annotation
+Imports SMRUCC.genomics.GCModeller.Assembly.GCMarkupLanguage.v2
+Imports SMRUCC.genomics.GCModeller.CompilerServices
+Imports SMRUCC.genomics.Metagenomics
+
+Public Class Compiler
 
     ReadOnly cad_registry As biocad_registry
-    ReadOnly template
+    ReadOnly template As GeneTable()
 
-    Sub New(registry As biocad_registry)
-
+    Sub New(registry As biocad_registry, genes As GeneTable())
+        template = genes
+        cad_registry = registry
     End Sub
 
+    Public Function CreateModel() As VirtualCell
+        Dim chromosome As New replicon
+
+        Return New VirtualCell With {
+            .properties = New [Property],
+            .taxonomy = New Taxonomy,
+            .genome = New Genome With {
+                .replicons = {chromosome}
+            }
+        }
+    End Function
 End Class
