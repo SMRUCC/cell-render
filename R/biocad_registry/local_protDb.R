382a1aed1c55283a8386aadd6136b14bef9cbba7
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-11-21 19:54:00 +0800

    pull the sequence data from database

--- R/biocad_registry/local_protDb.R
+++ R/biocad_registry/local_protDb.R
@@ -12,5 +12,29 @@
 #' used for join the ec_number list.
 #' 
 const local_protDb = function(cad_registry) {
+    let page_size = 1000;
+    let offset = 1;
+    let 
 
+    for(let page in tqdm(1:10000)) {
+        offset <- (page - 1) * page_size;
+        page <- cad_registry 
+        |> table("molecule") 
+        |> left_join("sequence_graph")
+        |> on(sequence_graph.molecule_id = molecule.id)
+        |> left_join("db_xrefs")
+        |> on(db_xrefs.obj_id = molecule.id, db_key in [77 , 79])
+        |> where(molecule.type = 3)
+        |> group_by("molecule.id")
+        |> limit(offset, page_size)
+        |> select(
+            "`molecule`.id",
+            "MIN(sequence) AS prot_seq",
+            "MIN(note) AS prot",
+            "GROUP_CONCAT(DISTINCT xref) AS ec_number")
+        ;
+
+        print(page);
+        stop();
+    }
 }
