356d1f2b124e94262513029c28ee20dd982d7db4
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-11-23 16:26:20 +0800

    how to decode the embedding graph

--- R/biocad_registry/reaction_pool.R
+++ R/biocad_registry/reaction_pool.R
@@ -16,7 +16,25 @@ const reaction_pool = function(cad_registry) {
             ;
 
         for(let r in as.list(page, byrow = TRUE) |> tqdm()) {
-            str(r);
+            page <- cad_registry |> table("reaction_graph") 
+                |> left_join("sequence_graph")
+                |> on(sequence_graph.molecule_id = reaction_graph.molecule_id)
+                |> where(reaction = r$id)
+                |> select(
+                    sequence_graph.molecule_id,
+                    db_xref,
+                    role,
+                    "sequence AS smiles",
+                    embedding
+                )
+                ;
+
+            if (any(nchar(page$embedding) == 0)) {
+                next;
+            }
+
+            print(page);
+            stop();
         }
     }
 }
