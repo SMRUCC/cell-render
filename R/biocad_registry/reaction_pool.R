2ec5f5f443fd8b2acc7fc6a4b1ad427b044c40a7
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-11-23 18:15:30 +0800

    save metabolite result

--- R/biocad_registry/reaction_pool.R
+++ R/biocad_registry/reaction_pool.R
@@ -1,10 +1,14 @@
 #' pull reaction data from cad registry to local
 #' 
-const reaction_pool = function(cad_registry) {
+const reaction_pool = function(cad_registry, repo) {
     let total = cad_registry |> table("reaction") |> count();
     let page_size = 1000;
     let total_pages = as.integer(total / page_size) + 1;
     let offset = 0;
+
+    require(HDS);
+
+    let local_repo = HDS::createStream(repo);
 
     for(let page in 1:total_pages) {
         print(`processing of data page: ${page}/${page_size}...`);
@@ -24,17 +28,19 @@ const reaction_pool = function(cad_registry) {
                     sequence_graph.molecule_id,
                     db_xref,
                     role,
-                    "sequence AS smiles",
-                    embedding
+                    "sequence AS smiles"                    
                 )
                 ;
 
-            if (any(nchar(page$embedding) == 0)) {
+            if (any(nchar(page$smiles) == 0)) {
                 next;
-            }
-
-            print(page);
-            stop();
+            } else {
+                let txt = file(`/reactions/${r$id}.csv`, truncate=TRUE, repo = local_repo); 
+                write.csv(page, file = txt);
+                close(txt);
+            }            
         }
     }
+
+    close(local_repo);
 }
--- test/pull_reactions.R
+++ test/pull_reactions.R
@@ -2,4 +2,4 @@ require(CellRender);
 
 let cad_registry = CellRender::open_registry("root", 123456, host = "192.168.3.233");
 
-cad_registry |> reaction_pool();
+cad_registry |> reaction_pool(repo = "Z:/zzzzz.hds");
