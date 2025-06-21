96579cd36ba6c1c76abc83db6ebaee3981f2af6a
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-06-21 19:21:52 +0800

    save data

---
+++ src/CellRender/CadLab.vb
@@ -0,0 +1,25 @@
+﻿
+Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports SMRUCC.genomics.Analysis.HTS.DataFrame
+Imports SMRUCC.Rsharp.Runtime.Interop
+
+<Package("cad_lab")>
+<RTypeExport("cad_lab", GetType(cad_lab))>
+Module CadLab
+
+    ''' <summary>
+    ''' 
+    ''' </summary>
+    ''' <param name="cad_lab"></param>
+    ''' <param name="exp_id">
+    ''' the experiment project id
+    ''' </param>
+    ''' <param name="dynaimics"></param>
+    ''' <returns></returns>
+    <ExportAPI("save_molecules")>
+    Public Function save_molecules(cad_lab As cad_lab, exp_id As String, dynaimics As Matrix)
+
+    End Function
+
+End Module
