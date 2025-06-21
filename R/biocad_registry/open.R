b66a93b5db57db9cfe101a094c3a90d346b717bd
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-06-21 19:52:53 +0800

    save simulation result into database

--- R/biocad_registry/open.R
+++ R/biocad_registry/open.R
@@ -14,3 +14,20 @@ const open_registry = function(user,passwd, host = "localhost",port = 3306) {
         port = port
     );
 }
+
+const open_cadlab = function(user,passwd, host = "localhost",port = 3306) {
+    require(graphQL);
+
+    # load mysql driver
+    imports "mysql" from "graphR";
+    imports "cad_lab" from "CellRender";
+
+    # create registry connection
+    mysql::open(
+        user_name = user,
+        password = passwd,
+        dbname = "cad_lab",
+        host  = host,
+        port = port
+    );
+}
---
+++ test/workspace_2024_ecoli/save_database.R
@@ -0,0 +1,10 @@
+require(GCModeller);
+
+imports "rawXML" from "vcellkit";
+imports "geneExpression" from "phenotype_kit";
+
+let rawdata = open.vcellPack(file = "./result.vcellPack", mode = "read");
+let metabolome = time.frames(rawdata, module="Metabolites", symbol_name= TRUE);
+let cad_lab = open_cadlab(user="root",passwd=123456, host = "biocad.cloud",port = 3306);
+
+cad_lab |> save_expression(exp_id = "Test-Ecoli-20250621", dynaimics = metabolome);
