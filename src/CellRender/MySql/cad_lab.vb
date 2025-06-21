5dc727f0b957fbecc0e48f40192494228300963a
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-06-21 18:24:27 +0800

    update mysql database model

---
+++ src/CellRender/MySql/cad_lab.vb
@@ -0,0 +1,28 @@
+﻿Imports CellRender.biocad_labModel
+Imports Oracle.LinuxCompatibility.MySQL
+Imports Oracle.LinuxCompatibility.MySQL.Uri
+
+Public Class cad_lab : Inherits biocad_labModel.db_cad_lab
+
+    Public ReadOnly Property dynamics As TableModel(Of dynamics)
+        Get
+            Return m_dynamics
+        End Get
+    End Property
+
+    Public ReadOnly Property experiment As TableModel(Of experiment)
+        Get
+            Return m_experiment
+        End Get
+    End Property
+
+    Public ReadOnly Property virtualcell As TableModel(Of virtualcell)
+        Get
+            Return m_virtualcell
+        End Get
+    End Property
+
+    Public Sub New(mysqli As ConnectionUri)
+        MyBase.New(mysqli)
+    End Sub
+End Class
