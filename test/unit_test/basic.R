44b8e8d0fb2392f4cb702b2d186a8101c32b817e
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-07-14 20:14:44 +0800

    Update basic.R

--- test/unit_test/basic.R
+++ test/unit_test/basic.R
@@ -1,23 +1,23 @@
 require(GCModeller);
 
+imports ["simulator" "compiler"] from "vcellkit";
+
 let vcell = compile_network(
-    A +2*B = 3*C+D,
-    A = 2*C + 2 * D,
-    B = 2 * C
+    A +2*B == 3*C+D,
+    A == 2*C + 2 * D,
+    B == 2 * C
 );
 
 vcell = vcell.model(vcell);
 
 let engine = vcell |> engine.load(	
 	inits = mass0(vcell, random = [100,5000]) |> set_status( 
-						 Intracellular = list(A = 120, B = 10, C = 0),
-						 Extracellular = list(A = 1200,   B = 0,  C = 0)
+						 Intracellular = list(A = 120, B = 10, C = 0, D = 100)
 	),
 	iterations       = 100, 
 	time_resolutions = 1000, 	
 	showProgress     = TRUE
 ) 
-|> set_data_driver(data.frame)
 |> run()
 ;
 
