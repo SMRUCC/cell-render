f21346b2fa6f5e3f08c2e59a30355664baa202d9
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-03-02 14:20:38 +0800

    run single cell analysis on embedding result

--- R/tools/embedding.R
+++ R/tools/embedding.R
@@ -40,11 +40,19 @@ const export_tree = function(embedding_file, workdir = "./", node_equals = 0.999
 const singlecells_analysis = function(embedding_file, workdir = "./") {
     let data = read.csv(embedding_file, row.names = 1, check.names = FALSE);
     let taxon = biom_string.parse(data$taxonomy);
+    let group_dir = NULL;
 
     for(rank in c("Phylum","Class","Order")) {
         data[, "taxonomy"] <- taxonomy_name(taxon, rank = rank) ;
+        group_dir = file.path(workdir, "singlecells", `group_${tolower(rank)}`);
 
-        write.csv(data, file = file.path(workdir, "singlecells", `group_${tolower(rank)}.csv`));
+        dir.create(group_dir);
+        native_r(singlecells_viz, list(
+            rawdata = data, 
+            outputdir = group_dir
+        ));
+
+        write.csv(data, file = file.path(group_dir, "group_data.csv"));
     }
 }
 
