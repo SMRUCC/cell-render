fd8333cdb4ee37e7b421e0c5c25189735691ae01
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-03-02 14:00:06 +0800

    make embedding

--- R/annotations/2.genome_list.R
+++
@@ -1,8 +0,0 @@
-require(GCModeller);
-
-imports ["annotation.workflow", "annotation.terms"] from "seqtoolkit";
-
-let stream = open.stream(relative_work("ec_terms.csv"),type = "terms", ioRead = TRUE);
-let genomes = make_vectors(stream, stream = TRUE);
-
-write_genomes_jsonl(genomes, file = relative_work("genomes_metabolic.jsonl"));
---
+++ R/tools/embedding.R
@@ -0,0 +1,26 @@
+require(GCModeller);
+require(umap);
+
+imports ["annotation.workflow", "annotation.terms"] from "seqtoolkit";
+imports "taxonomy_kit" from "metagenomics_kit";
+
+const diamond_embedding = function(diamond_result, workdir = "./", union_contigs = 250) {
+    let rawdata = read_m8(diamond_result, stream = TRUE);
+    let ec_terms = file.path(workdir, "ec_terms.csv");
+    let metabolic_data  = file.path(workdir, "genomes_metabolic.jsonl");
+    let ec_emebedding = file.path(workdir, "metabolic_embedding.csv");
+    let stream = open.stream(ec_terms, type = "terms");
+
+    stream.flush(m8_metabolic_terms(rawdata), stream);
+    stream = open.stream(ec_terms,type = "terms", ioRead = TRUE);
+
+    let genomes = make_vectors(stream, stream = TRUE);
+
+    write_genomes_jsonl(genomes, file = metabolic_data);
+
+    let models = read.jsonl(file = metabolic_data, what = "genome_vector");
+    let vec = models |> tfidf_vectorizer(union_contigs = union_contigs, hierarchical  = TRUE);
+
+    write.csv(vec, file = ec_emebedding );
+}
+
