c4f40cc5b15d82af62416fbed705300c74a9039b
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-03-20 16:48:45 +0800

    create genbank project

--- src/CellBuilder/Project/ProjectCreator.vb
+++ src/CellBuilder/Project/ProjectCreator.vb
@@ -6,64 +6,68 @@ Imports SMRUCC.genomics.ContextModel.Promoter
 Imports SMRUCC.genomics.Metagenomics
 Imports SMRUCC.genomics.SequenceModel.FASTA
 
-Public Module ProjectCreator
+Public Class ProjectCreator
+
+    Dim nucl As New Dictionary(Of String, String)
+    Dim prot As New Dictionary(Of String, String)
+    Dim genes As New List(Of GeneTable)
+    Dim size As New Dictionary(Of String, Integer)
+    Dim tax As Taxonomy = Nothing
+    Dim tss As New Dictionary(Of String, String)
+
+    Private Sub ScanReplicon(replicon As GBFF.File)
+        Dim nt As New FastaSeq({"nt"}, replicon.Origin.SequenceData)
+
+        If tax Is Nothing Then
+            tax = replicon.Source.GetTaxonomy
+        End If
+
+        Call replicon _
+            .EnumerateGeneFeatures(ORF:=False) _
+            .GroupBy(Function(a) a.Query(FeatureQualifiers.locus_tag)) _
+            .ToDictionary(Function(a) a.Key,
+                          Function(a)
+                              Return a.First.SequenceData
+                          End Function) _
+            .DoCall(Sub(list) Call nucl.AddRange(list, replaceDuplicated:=True))
+
+        Call replicon _
+            .ExportProteins_Short(True) _
+            .ToDictionary(Function(a) a.Title,
+                          Function(a)
+                              Return a.SequenceData
+                          End Function) _
+            .DoCall(Sub(list) Call prot.AddRange(list, replaceDuplicated:=True))
+
+        Dim geneSet As GeneTable() = replicon _
+            .EnumerateGeneFeatures(ORF:=False) _
+            .ExportTable _
+            .ToArray
+
+        Call genes.AddRange(geneSet)
+        Call size.Add(replicon.Locus.AccessionID,
+                      replicon.Origin.SequenceData.Length)
+
+        Dim gene_upstreamSet = geneSet _
+            .Select(Function(gene)
+                        Return (gene.locus_id, gene.GetUpstreamSeq(nt, 150))
+                    End Function) _
+            .ToArray
+
+        Call gene_upstreamSet _
+            .GroupBy(Function(a) a.locus_id) _
+            .ToDictionary(Function(a) a.Key,
+                          Function(a)
+                              Return Strings.UCase(a.First.Item2.SequenceData)
+                          End Function) _
+            .DoCall(Sub(list)
+                        Call tss.AddRange(list, replaceDuplicated:=True)
+                    End Sub)
+    End Sub
 
     Public Function FromGenBank(replicons As IEnumerable(Of GBFF.File)) As GenBankProject
-        Dim nucl As New Dictionary(Of String, String)
-        Dim prot As New Dictionary(Of String, String)
-        Dim genes As New List(Of GeneTable)
-        Dim size As New Dictionary(Of String, Integer)
-        Dim tax As Taxonomy = Nothing
-        Dim tss As New Dictionary(Of String, String)
-
         For Each replicon As GBFF.File In replicons
-            Dim nt As New FastaSeq({"nt"}, replicon.Origin.SequenceData)
-
-            If tax Is Nothing Then
-                tax = replicon.Source.GetTaxonomy
-            End If
-
-            Call replicon _
-                .EnumerateGeneFeatures(ORF:=False) _
-                .GroupBy(Function(a) a.Query(FeatureQualifiers.locus_tag)) _
-                .ToDictionary(Function(a) a.Key,
-                              Function(a)
-                                  Return a.First.SequenceData
-                              End Function) _
-                .DoCall(Sub(list) Call nucl.AddRange(list, replaceDuplicated:=True))
-
-            Call replicon _
-                .ExportProteins_Short(True) _
-                .ToDictionary(Function(a) a.Title,
-                              Function(a)
-                                  Return a.SequenceData
-                              End Function) _
-                .DoCall(Sub(list) Call prot.AddRange(list, replaceDuplicated:=True))
-
-            Dim geneSet As GeneTable() = replicon _
-                .EnumerateGeneFeatures(ORF:=False) _
-                .ExportTable _
-                .ToArray
-
-            Call genes.AddRange(geneSet)
-            Call size.Add(replicon.Locus.AccessionID,
-                          replicon.Origin.SequenceData.Length)
-
-            Dim gene_upstreamSet = geneSet _
-                .Select(Function(gene)
-                            Return (gene.locus_id, gene.GetUpstreamSeq(nt, 150))
-                        End Function) _
-                .ToArray
-
-            Call gene_upstreamSet _
-                .GroupBy(Function(a) a.locus_id) _
-                .ToDictionary(Function(a) a.Key,
-                              Function(a)
-                                  Return Strings.UCase(a.First.Item2.SequenceData)
-                              End Function) _
-                .DoCall(Sub(list)
-                            Call tss.AddRange(list, replaceDuplicated:=True)
-                        End Sub)
+            Call ScanReplicon(replicon)
         Next
 
         Dim proj As New GenBankProject With {
@@ -77,4 +81,4 @@ Public Module ProjectCreator
 
         Return proj
     End Function
-End Module
+End Class
---
+++ src/CellRender/builder.vb
@@ -0,0 +1,31 @@
+﻿Imports CellBuilder
+Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports SMRUCC.genomics.Assembly.NCBI.GenBank
+Imports SMRUCC.Rsharp.Runtime
+Imports SMRUCC.Rsharp.Runtime.Internal.[Object]
+Imports SMRUCC.Rsharp.Runtime.Interop
+
+<Package("project")>
+<RTypeExport("genbank_project", GetType(GenBankProject))>
+Module ProjectBuilder
+
+    ''' <summary>
+    ''' 
+    ''' </summary>
+    ''' <param name="replicons">a vector of the ncbi genbank object of the genome replicons</param>
+    ''' <param name="env"></param>
+    ''' <returns></returns>
+    <ExportAPI("new")>
+    <RApiReturn(GetType(GenBankProject))>
+    Public Function create(<RRawVectorArgument> replicons As Object, Optional env As Environment = Nothing) As Object
+        Dim pull As pipeline = pipeline.TryCreatePipeline(Of GBFF.File)(replicons, env)
+
+        If pull.isError Then
+            Return pull.getError
+        End If
+
+        Return New ProjectCreator().FromGenBank(pull.populates(Of GBFF.File)(env))
+    End Function
+
+End Module
