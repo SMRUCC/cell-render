50e3c943d483fb58ef1301bf8496929d25c87078
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-03-20 16:13:29 +0800

    move model to cell builder project

---
+++ src/CellBuilder/Annotations/ECNumberAnnotation.vb
@@ -0,0 +1,8 @@
+﻿Public Class ECNumberAnnotation
+
+    Public Property gene_id As String
+    Public Property EC As String
+    Public Property Score As Double
+    Public Property SourceIDs As String()
+
+End Class
