8d5b6404876de26b80142dedd4225fc9d89145c7
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-04-01 02:34:45 +0800

    how to make the genbank project

---
+++ R/annotations/make_genbank_proj.R
@@ -0,0 +1,9 @@
+#' Extract the genbank source
+#' 
+#' @param app the current workflow app object
+#' @param context the workflow context object
+#' 
+[@app "make_genbank_proj"]
+const make_genbank_proj = function(app, context) {
+    
+}
--- R/apps.R
+++ R/apps.R
@@ -5,7 +5,7 @@
 const annotation_workflow = function() {
     print("Config the cellular graph network annotation & modelling workflow...");
 
-    WorkflowRender::hook(app("extract_genbank_src", extract_gene_table));
+    WorkflowRender::hook(make_genbank_proj);
     WorkflowRender::hook(app("extract_tfbs_motifs", tfbs_motif_scanning));
     WorkflowRender::hook(app("assemble_TRN", assemble_transcript_graph));
     WorkflowRender::hook(app("assemble_RXN", assemble_metabolic_graph));
