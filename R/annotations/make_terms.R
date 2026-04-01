573df0808fd741614a0f110418a4bc4768114b5f
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-04-01 12:08:56 +0800

    make annotation terms

--- R/annotations/make_terms.R
+++ R/annotations/make_terms.R
@@ -1,7 +1,15 @@
+imports "annotation.workflow" from "seqtoolkit";
+
 [@app "make_terms"]
 const make_terms = function(app, context) {
-    let ec_number = workfile("make_diamond_hits://ec_number.m8");
-    let subcellular = workfile("make_diamond_hits://subcellular.m8");
-    let tf_list = workfile("make_diamond_hits://transcript_factor.m8");
-    
+    let ec_number = read_m8(workfile("make_diamond_hits://ec_number.m8"));
+    let subcellular = read_m8(workfile("make_diamond_hits://subcellular.m8"));
+    let tf_list = read_m8(workfile("make_diamond_hits://transcript_factor.m8"));
+    let proj = project::load(workfile("make_genbank_proj://builder.gcproj"));
+
+    ec_number |> diamond_hitgroups |> set_blastp_result(proj, "ec_number");
+    subcellular |> diamond_hitgroups |> set_blastp_result(proj, "subcellular_location");
+    tf_list |> diamond_hitgroups |> set_blastp_result(proj, "transcript_factor");
+
+    project::save(proj, file = workfile(app, "builder.gcproj"));
 }
--- src/CellRender/workflow.vb
+++ src/CellRender/workflow.vb
@@ -3,9 +3,11 @@ Imports CellBuilder
 Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.Scripting.MetaData
 Imports SMRUCC.genomics.Analysis.SequenceTools.SequencePatterns
+Imports SMRUCC.genomics.Interops.NCBI.Extensions.Tasks.Models
 Imports SMRUCC.genomics.SequenceModel.FASTA
 Imports SMRUCC.Rsharp.Runtime
 Imports SMRUCC.Rsharp.Runtime.Components
+Imports SMRUCC.Rsharp.Runtime.Internal.[Object]
 Imports SMRUCC.Rsharp.Runtime.Interop
 Imports SMRUCC.Rsharp.Runtime.Vectorization
 Imports RInternal = SMRUCC.Rsharp.Runtime.Internal
@@ -29,6 +31,13 @@ Module workflow
             .ToArray
     End Function
 
+    ''' <summary>
+    ''' extract of the protein fasta sequence data to file
+    ''' </summary>
+    ''' <param name="proj"></param>
+    ''' <param name="file"></param>
+    ''' <param name="env"></param>
+    ''' <returns></returns>
     <ExportAPI("save_proteins")>
     Public Function save_proteins(proj As GenBankProject, file As Object, Optional env As Environment = Nothing) As Object
         Dim s = SMRUCC.Rsharp.GetFileStream(file, FileAccess.Write, env)
@@ -38,6 +47,29 @@ Module workflow
         End If
 
         Call proj.DumpProteinFasta(s.TryCast(Of Stream))
+
+        Return Nothing
+    End Function
+
+    <ExportAPI("set_blastp_result")>
+    Public Function set_blastp_result(<RRawVectorArgument> blastp_hits As Object, proj As GenBankProject, group As String, Optional env As Environment = Nothing) As Object
+        Dim pull As pipeline = pipeline.TryCreatePipeline(Of HitCollection)(blastp_hits, env)
+
+        If pull.isError Then
+            Return pull.getError
+        ElseIf proj.annotations Is Nothing Then
+            proj.annotations = New AnnotationSet
+        End If
+
+        Dim protein_hits As HitCollection() = pull.populates(Of HitCollection)(env).ToArray
+
+        Select Case Strings.LCase(group)
+            Case "ec_number" : proj.annotations.enzyme_hits = protein_hits
+            Case "subcellular_location" : proj.annotations.transporter = protein_hits
+            Case "transcript_factor" : proj.annotations.tf_hits = protein_hits
+            Case Else
+                Return RInternal.debug.stop($"unknown annotation group of '{group}'", env)
+        End Select
 
         Return Nothing
     End Function
