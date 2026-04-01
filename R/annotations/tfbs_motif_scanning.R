025829d306c26db4c559c14a74e6748ab87486e4
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-04-01 15:58:02 +0800

    Update tfbs_motif_scanning.R

--- R/annotations/tfbs_motif_scanning.R
+++ R/annotations/tfbs_motif_scanning.R
@@ -7,11 +7,21 @@ imports "bioseq.patterns" from "seqtoolkit";
 const tfbs_motif_scanning = function(app, context) {    
     let upstream_seq = workfile("make_genbank_proj://upstream_locis.fasta");
     let motifs_db = file.path(get_config("localdb"), "motifs");
-    let motifs = GCModeller::scan_motifs(motifs_db, upstream_seq, 
+    let motifs = GCModeller::scan_motifs(
+        db = motifs_db, 
+        seqs = upstream_seq, 
         workdir = workfile(app, "tfbs_sites"), 
-        n_threads = get_config("n_threads")
+        n_threads = get_config("n_threads"),
+        identities_cutoff = 0.9
     );
     let outfile = workfile(app, "tfbs_motifs.csv");
+
+    # seeds 
+    # $"{motif.id} {motif.family} [{motif.name}]"
+    #
+    #         motif.id family [name]
+    #              --- ---- -------------------   
+    # "AsnC; MOTIF 557 AsnC [YwrC - Bacillales] Motif_1" 
 
     write.csv(motifs, file = outfile, row.names = FALSE);
 }
