504ca8ca0f7afe4bcb07e3a33b9b94be22327c9a
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-04-01 21:48:02 +0800

    make annotation

---
+++ src/CellBuilder/Annotations/Annotation.vb
@@ -0,0 +1,71 @@
+﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.Linq
+Imports SMRUCC.genomics.Interops.NCBI.Extensions.LocalBLAST.Application.BBH
+Imports SMRUCC.genomics.Interops.NCBI.Extensions.Tasks.Models
+
+Public Module Annotation
+
+    <Extension>
+    Public Function AssignECNumber(enzymeHits As HitCollection) As ECNumberAnnotation
+        Dim enzymes = enzymeHits.AsEnumerable.GroupBy(Function(a) a.hitName.Split("|"c).First).ToArray
+        Dim enzyme_scores As ECNumberAnnotation() = enzymes _
+            .Select(Function(a)
+                        Dim ec_number = a.Key
+                        Dim total As Double = Aggregate hit As Hit In a Into Sum(hit.score * hit.identities * hit.positive)
+                        Dim source_id As String() = a.Select(Function(prot) prot.hitName.GetTagValue("|"c).Value).Distinct.ToArray
+
+                        Return New ECNumberAnnotation With {
+                            .EC = ec_number,
+                            .Score = total,
+                            .SourceIDs = source_id,
+                            .gene_id = enzymeHits.QueryName
+                        }
+                    End Function) _
+            .ToArray
+
+        If enzyme_scores.Length = 0 Then
+            Return Nothing
+        ElseIf enzyme_scores.Length = 1 Then
+            Return enzyme_scores(0)
+        Else
+            Return enzyme_scores _
+                .OrderByDescending(Function(a) a.Score) _
+                .First
+        End If
+    End Function
+
+    <Extension>
+    Public Function AssignTFFamilyHit(tfhits As HitCollection) As BestHit
+        Dim enzymes = tfhits.AsEnumerable.GroupBy(Function(a) a.hitName.Split("|"c).First).ToArray
+        Dim enzyme_scores As BestHit() = enzymes _
+            .Select(Function(a)
+                        Dim ec_number = a.Key
+                        Dim total As Double = Aggregate hit As Hit In a Into Sum(hit.score * hit.identities * hit.positive)
+                        Dim source_id As String() = a.Select(Function(prot) prot.hitName.GetTagValue("|"c).Value).Distinct.ToArray
+
+                        Return New BestHit With {
+                            .HitName = ec_number,
+                            .hit_length = a.Count,
+                            .score = total,
+                            .description = source_id.JoinBy(", "),
+                            .QueryName = tfhits.QueryName,
+                            .identities = a.Average(Function(i) i.identities),
+                            .evalue = a.Average(Function(i) i.evalue),
+                            .positive = a.Average(Function(i) i.positive)
+                        }
+                    End Function) _
+            .ToArray
+
+        If enzyme_scores.Length = 0 Then
+            Return Nothing
+        ElseIf enzyme_scores.Length = 1 Then
+            Return enzyme_scores(0)
+        Else
+            Return enzyme_scores _
+                .OrderByDescending(Function(a) a.score) _
+                .First
+        End If
+    End Function
+
+End Module
+
--- src/CellBuilder/Annotations/ECNumberAnnotation.vb
+++ src/CellBuilder/Annotations/ECNumberAnnotation.vb
@@ -1,8 +1,20 @@
-﻿Public Class ECNumberAnnotation
+﻿Imports SMRUCC.genomics.Interops.NCBI.Extensions.Tasks.Models
+
+Public Class ECNumberAnnotation
 
     Public Property gene_id As String
     Public Property EC As String
     Public Property Score As Double
     Public Property SourceIDs As String()
+
+    Public Shared Iterator Function MakeEnzymeTerms(blastp As IEnumerable(Of HitCollection)) As IEnumerable(Of ECNumberAnnotation)
+        For Each protein As HitCollection In blastp
+            Dim ec As ECNumberAnnotation = protein.AssignECNumber()
+
+            If Not ec Is Nothing Then
+                Yield ec
+            End If
+        Next
+    End Function
 
 End Class
