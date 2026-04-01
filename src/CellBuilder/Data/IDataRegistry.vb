8d1b6236d4610c66e7040319205d8d08841f4dcc
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-04-01 22:33:44 +0800

    create registry

--- src/CellBuilder/Data/IDataRegistry.vb
+++ src/CellBuilder/Data/IDataRegistry.vb
@@ -1,3 +1,15 @@
-﻿Public Interface IDataRegistry
+﻿Imports SMRUCC.genomics.GCModeller.ModellingEngine.Model.WebJSON
+
+Public Interface IDataRegistry
+
+    Function GetMoleculeDataByID(id As UInteger) As Molecule
+    Function GetAssociatedReactions(ec_number As String, Optional simple As Boolean = False) As Dictionary(Of String, Reaction)
+
+    ''' <summary>
+    ''' 
+    ''' </summary>
+    ''' <param name="registry_id">id of the molecule</param>
+    ''' <returns></returns>
+    Function ExpandNetworkByCompound(registry_id As String) As Dictionary(Of String, Reaction)
 
 End Interface
