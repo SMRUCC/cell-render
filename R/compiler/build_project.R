d500150bd6a180a8d6ec094264c9c683e5afc9b0
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-04-01 23:01:25 +0800

    Update build_project.R

--- R/compiler/build_project.R
+++ R/compiler/build_project.R
@@ -1,5 +1,16 @@
 #' build project into the virtual cell model
 [@app "build_project"]
 const build_project = function(app, context) {
+    let proj = project::load(get_config("proj_file"));
+    let registry = open_datapool(dir = get_config("localdb"));
+    let vcell_name = get_config("vcell_name");
+    let vcell = proj |> build(
+        datapool   = registry, 
+        vcell_name = vcell_name
+    );
 
+    vcell 
+    |> xml 
+    |> writeLines(con = get_config("model_file"))
+    ; 
 }
