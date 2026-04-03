dc33fc27da54bb9a0d91a49fb1f1d618323c051b
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-04-04 02:14:04 +0800

    Update pangenome.R

--- R/landscape/pangenome.R
+++ R/landscape/pangenome.R
@@ -1,3 +1,8 @@
-const pangenome = function() {
-    
+#' pan-genome analysis of the microbial community
+#' 
+#' @param src a character vector of the directory path that contains the ncbi genbank assembly files.
+#' @param result_dir a character vector of the directory path for export the analysis result files. 
+#' 
+const pangenome = function(src, result_dir) {
+
 }
