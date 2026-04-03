22b1a65babb7c11a2c5e13bea38502bf745703b9
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-04-04 03:53:45 +0800

    Update pangenome.R

--- R/landscape/pangenome.R
+++ R/landscape/pangenome.R
@@ -1,4 +1,6 @@
 imports "pangenome" from "comparative_toolkit";
+imports "annotation.workflow" from "seqtoolkit";
+imports "annotation.terms" from "seqtoolkit";
 
 #' pan-genome analysis of the microbial community
 #' 
@@ -9,16 +11,43 @@ imports "pangenome" from "comparative_toolkit";
 const pangenome = function(src, result_dir, diamond = Sys.which("diamond"), n_threads = 32) {
     let source_dir = file.path(result_dir, "source");
     let blastp_dir = file.path(result_dir, "blastp");
-    
+
     # make export of the genomics protein fasta sequence and
     # gene annotation data as files
     extract_genomes(src, outputdir = source_dir);
+    # run protein annotation search via diamond blastp search
     batch_diamond(source_dir, blastp_dir, 
         diamond   = unlist(diamond), 
         n_threads = n_threads);
     
+    # make the pan-genome analysis at here
+    let links <- new ortho_groups();
 
+    # scan for all diamond blastp result files
+    for(let align_file in tqdm(list.files(blastp_dir, pattern = "*.m8"))) {
+        # read the diamond blastp alignment file
+        align_file 
+        |> read_m8()
+        # and then set orthology group for make gene family
+        |> assign_terms(top_best = TRUE, filter_unknown = TRUE, identities_cut = 30)
+        |> set_ortho_group(uf = links)
+        ;
+    }
 
+    let genome_files = list.files(source_dir, pattern = "*.csv");
+    let bin = pangenome::build_context(
+        genomes = as.list(genome_files, names = basename(genome_files)) 
+                    |> lapply(file => read_genetable(file))
+    );
+    # run pan-genome analysis
+    let result = bin |> pangenome::analysis(orthologSet = links);
+    
+    # export the analysis result to the result dir
+    writeLines(pangenome::report_html(result), con = file.path(result_dir, "analysis_report.html"));
+    write.csv(as.data.frame(pangenome::sv_table(result)), file = file.path(result_dir, "SV_result.csv"));
+    write.csv(as.data.frame(pangenome::pav_table(result)), file = file.path(result_dir, "PAV_result.csv"));
+
+    invisible(NULL);
 }
 
 #' make diamond blastp search against the reference enzyme database in batch
