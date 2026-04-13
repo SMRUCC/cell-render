2c4b811bf144df3d8fbc1dec767e9dffbcaf4799
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-04-13 14:02:32 +0800

    make gpr link

---
+++ src/CellBuilder/Compiler/GPRWorker.vb
@@ -0,0 +1,16 @@
+﻿Imports SMRUCC.genomics.GCModeller.Assembly.GCMarkupLanguage.v2
+Imports SMRUCC.genomics.GCModeller.CompilerServices.GPRLink
+
+Public Class GPRWorker
+
+    ReadOnly worker As MetabolicAssociator
+
+    Sub New(proj As GenBankProject)
+
+    End Sub
+
+    Public Function CreateMetabolismNetwork(genes As Dictionary(Of String, gene)) As MetabolismStructure
+
+    End Function
+
+End Class
