8c3817dea6af0bc962e000a6aa42486ea7374a9f
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-06-06 21:28:55 +0800

    Create MAGs_model.R

---
+++ R/MAGs/MAGs_model.R
@@ -0,0 +1,9 @@
+#' Compile the MAGs as virtual cell model
+#' 
+#' @param contigs a collection of the MAGs contigs assembly
+#' @param tax_str taxonomy string of this MAGs contigs assembly data, example as:
+#'    d__Archaea;p__Halobacteriota;c__Methanosarcinia;o__Methanosarcinales;f__Methanosarcinaceae;g__Methanosarcina;s__Methanosarcina sp001714685
+#'
+const MAGs_model = function(contigs, tax_str) {
+
+}
