c9360f8b9df32156b038fe99716f9850318f5cef
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-06-10 06:54:34 +0800

    config run test

---
+++ test/test_terms.R
@@ -0,0 +1,5 @@
+require(CellRender);
+
+ make_blastp_term(proj_file = "\\192.168.3.15\sda\20250606_ecoli\virtualcell\acetoin\20260610-1\release\NZ_CP034099\builder.gcproj", 
+    model_dir = "\\192.168.3.15\sda\20250606_ecoli\virtualcell\acetoin\20260610-1\tmp\workflow_tmp\make_diamond_hits\NZ_CP034099"
+);
