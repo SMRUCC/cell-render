e33f3b660af580f163c85958560ca4c5b7b993ee
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-06-25 15:25:28 +0800

    Update metaTraits.R

--- R/MAGs/metaTraits.R
+++ R/MAGs/metaTraits.R
@@ -5,7 +5,7 @@ imports "metaTraits" from "metagenomics_kit";
 const metaTraits = function(diamond) {
     let pfam = read_m8(diamond);
     let traitar = metaTraits::load.trait_models(repo = `${@datadir}/phypat+PGL/`);
-    let votes = metaTraits::make_predicts(pfam, bit_score = 25, evalue = 0.01);
+    let votes = metaTraits::make_predicts(traitar ,pfam, bit_score = 25, evalue = 0.01);
     let pheno_traits = metaTraits::phenotype_result(votes, models = traitar );
 
     return(pheno_traits);
