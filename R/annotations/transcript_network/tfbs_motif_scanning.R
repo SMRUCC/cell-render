b9b8910306955803babf2c593f75646b250c0f58
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-07-06 12:10:32 +0800

    Update tfbs_motif_scanning.R

--- R/annotations/transcript_network/tfbs_motif_scanning.R
+++ R/annotations/transcript_network/tfbs_motif_scanning.R
@@ -54,37 +54,49 @@ imports "bioseq.patterns" from "seqtoolkit";
 #' @export
 [@app "tfbs_motif_scanning"]
 const tfbs_motif_scanning = function(app, context) {    
+    # run this diamond blastp alignment workflow in batch process mode?
+    let batch_process = as.logical(get_config("batch_process"));
+    let n_threads = get_config("n_threads");
+
     # run motif site scanning if the TRN_network module build in virtual cell is enabled
     if (check_build_module("TRN_network")) {
-        # set the TSS upstream region site fasta file
-        let upstream_seq = workfile("make_genbank_proj://upstream_locis.fasta");
-        # get the motif database directory that contains multiple meme motif model files
-        let motifs_db = file.path(get_config("localdb"), "motifs", get_config("domain"));
-        # make TFBS site scanning on the TSS upstream region sites
-        # search site against the reference motif search.
-        let motifs = GCModeller::scan_motifs(
-            db = motifs_db, 
-            seqs = upstream_seq, 
-            workdir = workfile(app, "tfbs_sites"), 
-            n_threads = get_config("n_threads"),
-            identities_cutoff = 0.9
-        );
-        let outfile = workfile(app, "tfbs_motifs.csv");
+        if (batch_process) {
+            let source_dir = WorkflowRender::workspace("make_genbank_proj");
 
-        # seeds 
-        # $"{motif.id} {motif.family} [{motif.name}]"
-        #
-        #         motif.id family [name]
-        #              --- ---- -------------------   
-        # "AsnC; MOTIF 557 AsnC [YwrC - Bacillales] Motif_1" 
+            for(let model_dir in list_batch_models()) {
+                stop(model_dir);
+            }
+        } else {
+            # set the TSS upstream region site fasta file
+            let upstream_seq = workfile("make_genbank_proj://upstream_locis.fasta");
+            # get the motif database directory that contains multiple meme motif model files
+            let motifs_db = file.path(get_config("localdb"), "motifs", get_config("domain"));
+            # make TFBS site scanning on the TSS upstream region sites
+            # search site against the reference motif search.
+            let motifs = GCModeller::scan_motifs(
+                db = motifs_db, 
+                seqs = upstream_seq, 
+                workdir = workfile(app, "tfbs_sites"), 
+                n_threads = get_config("n_threads"),
+                identities_cutoff = 0.9
+            );
+            let outfile = workfile(app, "tfbs_motifs.csv");
 
-        # TF 
-        #
-        #   TF.name motif.id protein_model
-        #    ------ ----     --------------
-        #   >jk0144 1269 Protein:144-jk0144
+            # seeds 
+            # $"{motif.id} {motif.family} [{motif.name}]"
+            #
+            #         motif.id family [name]
+            #              --- ---- -------------------   
+            # "AsnC; MOTIF 557 AsnC [YwrC - Bacillales] Motif_1" 
 
-        write.csv(motifs, file = outfile, row.names = FALSE);
+            # TF 
+            #
+            #    TF.name motif.id protein_model
+            #     ------ ----     --------------
+            #   > jk0144 1269 Protein:144-jk0144
+
+            write.csv(motifs, file = outfile, row.names = FALSE);
+        }
     }
 }
 
