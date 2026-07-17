6cee2dfe029fcc205be49544bdcb6ae9b6ab73e9
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-07-17 15:28:14 +0800

    Update sequential_batch.R

--- R/compiler/sequential_batch.R
+++ R/compiler/sequential_batch.R
@@ -1,3 +1,17 @@
-const sequential_batch = function(src, outputdir = NULL, args = list()) {
+const sequential_batch = function(src, outputdir = "./", args = list()) {
+    let workdir = file.path(outputdir, "tmp");
+    let release_dir = get_config("release");    
+    let genbank_files = list.files(get_config("src"), 
+                                pattern = c("*.gb","*.gbk","*.gbff"), 
+                                recursive = TRUE);
+    
+    message(`Build virtualcell community model based ${length(genbank_files)} genbank source files!`);
 
+    for(let file in genbank_files) {
+        file |> make_genbank_proj_file(
+            release_dir = release_dir,
+            workdir = workdir,
+            batch_process = TRUE
+        );
+    }
 }
